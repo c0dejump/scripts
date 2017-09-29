0a7ced3ee7b521fd83ea2f3eccb658ff9174d37d
c0dejump <codejumpgame@gmail.com>
2017-09-29 12:03:11 +0200

    Create scurl.py
    mini scan url (dirb-like)

---
+++ scurl.py
@@ -0,0 +1,36 @@
+import requests
+import sys
+
+url = sys.argv[1]
+
+def tryUrl():
+	dico = raw_input("entrez un dico : ")
+	try:
+		global payload 
+		payload = open(dico,"r").read().split("\n")
+	except:
+		print "dico introuvable"
+		tryUrl()
+	for payl in payload:
+		link = url + payl
+		req = requests.get(link)
+		status_link = req.status_code
+		sys.stdout.write(" "+payl+"\r")
+		sys.stdout.flush()
+		if status_link == 200:
+			print "[+] " + link
+
+r = requests.get(url)
+stat = r.status_code
+print url
+if stat == 200:
+	print "url found"
+	tryUrl()
+elif stat == 301:
+	print "HTTP/2.0 301 Moved Permanently"
+	tryUrl()
+elif stat == 302:
+	print "Moved Temporarily"
+	tryUrl()
+else:
+	print "url not found"
