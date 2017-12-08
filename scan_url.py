b114ba7667025b30eeb327a3de5d91d4a39a1318
c0dejump <codejumpgame@gmail.com>
2017-12-08 13:54:29 +0100

    Update scan_url.py

--- scan_url.py
+++ scan_url.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import time
 
 url = sys.argv[1]
 
@@ -9,11 +10,12 @@ def tryUrl():
 		global payload 
 		payload = open(dico,"r").read().split("\n")
 	except:
-		print "dico introuvable"
+		print "dico not found"
 		tryUrl()
 	for payl in payload:
 		link = url + payl
 		req = requests.get(link)
+		time.sleep(0.2)
 		status_link = req.status_code
 		sys.stdout.write(" "+payl+"\r")
 		sys.stdout.flush()
@@ -21,6 +23,7 @@ def tryUrl():
 			print "\033[32m[+] \033[0m" + link
 		if status_link == 403:
 			print "\033[33m[+] \033[0m" + link + "\033[33m forbidden \033[0m"
+
 
 r = requests.get(url)
 stat = r.status_code
@@ -34,5 +37,11 @@ elif stat == 301:
 elif stat == 302:
 	print "Moved Temporarily"
 	tryUrl()
+elif stat == 404:
+        a = raw_input("not found/ forced ?(y:n)")
+        if a == "y":
+            tryUrl()
+        else:
+            sys.exit()
 else:
 	print "url not found"
