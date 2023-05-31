1f5463998e369c1c0858a7f3b35e76dc49324ab1
c0dejump <codejumpgame@gmail.com>
2023-05-31 11:16:54 +0200

    Add files via upload

---
+++ drupal_node.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+from lxml.html import fromstring
+import argparse
+
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+
+def main(url, type_, s):
+    type_ = "taxonomy/term/" if type_ == "taxonomy" else "node/"
+    for i in range(0,1000):
+        uri = "{}{}{}".format(url, type_, i)
+        req = s.get(uri, verify=False)
+        if req.status_code not in [404] and len(req.content) != 174921:
+            tree = fromstring(req.content)
+            title = tree.findtext('.//title')
+            print("\033[32m{}\033[0m - [{}b] - {} :: \033[34m{}\033[0m".format(req.status_code, len(req.content), uri, title))
+        sys.stdout.write(" {} \r".format(uri))
+        sys.stdout.write("\033[K")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-u", help="URL login to test \033[31m[required]\033[0m", dest='url')
+    parser.add_argument("-t", help="type of scan (taxonomy or node)", dest='type_', required=False)
+    results = parser.parse_args()
+                                     
+    url = results.url
+    type_ = results.type_
+
+    s = requests.Session()
+    s.headers.update({'User-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; LCJB; rv:11.0) like Gecko'})
+
+
+    main(url, type_, s)
